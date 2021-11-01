78786175501d9eff69eae0ed5d14168ac22bb55d
JulianBarbossa9 <julianbarbossa@hotmail.com>
2021-10-31 20:46:41 -0500

    verificación de info: creacion de state (guardarPresupuesto guardarPresupuesto)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Pregunta from './components/Pregunta';
 
 
+
 function App() {
+  /**
+   * Definir el state
+   */
+  const [presupuesto, guardarPresupuesto] = useState(0);
+  const [restante , guardarRestante ] = useState(0); 
+
+
+  
   return (
       <div className="container">
           <header>
             <h1>Gastos Semanales</h1>
             
             <Pregunta
+            guardarPresupuesto={guardarPresupuesto}
+            guardarRestante={guardarRestante}
             />
           </header>
       </div>
--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,17 +1,69 @@
-import React,{Fragment} from 'react';
+import React,{Fragment, useState} from 'react';
+import Error from './Error';
 
-const Pregunta = () => {
+
+
+
+const Pregunta = ({guardarPresupuesto, guardarRestante}) => {
+   
+   /**
+    * Definir el state
+    */
+   const [cantidad, guardarCantidad] = useState(0);
+
+   /**
+    *  Verificar info dada
+    */
+   const [error, guardarError] = useState(false);
+
+   /**
+    * Función que lee el presupuesto
+    */
+   const definiPresupuesto = e => {
+       //Convierto el string a un valor
+       guardarCantidad(parseInt(e.target.value,10));
+   }
+   
+   /**
+    * Submit para definir presupuesto
+    */
+    
+   const enviarInfo = e =>{
+       // preventDefault --> sirve para que no envie el query ("d/usuario/documentos") y tampoco recargue la pagina
+       e.preventDefault();
+
+       // Validar la info
+        if(cantidad < 1 || isNaN (cantidad)){
+            guardarError(true);
+            return;
+        }
+
+        guardarError(false);
+
+       // Que se hace si pasa la validación 
+        guardarPresupuesto(cantidad);
+        guardarRestante(cantidad);
+   }
+
     return (  
         <Fragment>
             <div className="presupuesto">
 
                     <div className="containerInput">
-                    <h2 className="titlePresupuesto">Digite su Presupuesto</h2>
-                        <form>
+                        <h2 className="titlePresupuesto">Digite su Presupuesto</h2>
+                            
+                            <div className="error-msg">
+                                {error ? <Error mensaje="El Presupuesto es Incorrecto"/> : null}
+                            </div>
+
+                        <form
+                            onSubmit = {enviarInfo}
+                        >
                             <input
                                 type="number"
                                 className="presupuestoIni"
                                 placeholder="Digite su presupuesto"
+                                onChange = {definiPresupuesto}
                             />
 
                             <input
